d4d2ef20f086f7e15f5bd5b6bfc3db3a7bd86198
Stefi-C <cristinistif000@icloud.com>
2024-07-19 15:44:04 +0200

    responsive media query

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-
-        
+           //learn more button
+          /*
+           function myFunction() {
+            var dots = document.getElementsByClassName("dots");
+            var moreText = document.getElementsByClassName("more");
+            var btnText = document.getElementsByClassName("myBtn");
+            if(dots){if (dots.style.display === "none") {
+              dots.style.display = "inline";
+              btnText.innerHTML = "Read more";
+              moreText.style.display = "none";
+            } else {
+              dots.style.display = "none";
+              btnText.innerHTML = "Read less";
+              moreText.style.display = "inline";
+            }}
+            
+           }
+        */ //need to fix it
            
         
             
@@ -9,4 +25,3 @@
          
            
             
-        
