0cffaf9c462a3309a7a00d421f89abf4be859752
Stefi-C <cristinistif000@icloud.com>
2024-07-19 14:45:05 +0200

    ...

--- script.js
+++ script.js
@@ -1,27 +1,5 @@
 
-        //navbar
-        const navbarBtn = document.querySelectorAll(".toggle_btn"),
-        navbar = document.querySelectorAll('.navbar'),
-        icon = document.querySelectorAll('.fa-bars');
         
-         
-                navbarBtn.addEventListener('click', function(){
-    console.log("Toggle button clicked");
-            if(window.innerWidth < 1024){    
-                if (navbar.style.top === "0px" || navbar.style.top === "") {
-                    console.log("Hiding navbar");
-                    
-                    icon.classList.replace("fa-times", "fa-bars");
-                    
-                } else if(navbar.style.top === "-900px") {
-                    console.log("Showing navbar");
-                    navbar.style.top = "0px";
-                    icon.classList.replace("fa-bars", "fa-times");
-                    
-                }
-               
-            }
-            })
            
         
             
