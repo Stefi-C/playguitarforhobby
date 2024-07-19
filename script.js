c4f579c788311200de283d0d435f9ef44d20356f
Stefi-C <cristinistif000@icloud.com>
2024-07-19 14:37:37 +0200

    fixed bug menu navbar

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
 
         //navbar
-        const navbarBtn = document.querySelector(".toggle_btn"),
-        navbar = document.querySelector('.navbar'),
-        icon = document.querySelector('.fa-bars');
-        if(window.innerWidth < 1024){
-            navbar.style.top = '-900px';
-             function navbarFunction(){
-                console.log("Toggle button clicked");
+        const navbarBtn = document.querySelectorAll(".toggle_btn"),
+        navbar = document.querySelectorAll('.navbar'),
+        icon = document.querySelectorAll('.fa-bars');
+        
+         
+                navbarBtn.addEventListener('click', function(){
+    console.log("Toggle button clicked");
+            if(window.innerWidth < 1024){    
                 if (navbar.style.top === "0px" || navbar.style.top === "") {
                     console.log("Hiding navbar");
-                    navbar.style.top = "-900px";
                     
-                } else {
-                    console.log("Showing navbar");
-                    navbar.style.top = "0";
-                    
-                }
-                if(icon.classList.contains("fa-bars")){
-                    icon.classList.replace("fa-bars", "fa-times");
-                }
-                else{
                     icon.classList.replace("fa-times", "fa-bars");
+                    
+                } else if(navbar.style.top === "-900px") {
+                    console.log("Showing navbar");
+                    navbar.style.top = "0px";
+                    icon.classList.replace("fa-bars", "fa-times");
+                    
                 }
+               
             }
-        }
+            })
+           
+        
+            
+      
+        
+            
+         
            
             
         
